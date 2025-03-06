c3842100f7b9adcb6445e75b900cd8bce7811039
Bashir <bashirahmed.cse@gmail.com>
2025-03-07 01:51:05 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Generate static HTML files
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
